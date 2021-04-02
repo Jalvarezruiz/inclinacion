6e6d645fef8261e3d7ffa0dc30b62a5c8e10afe7
Jesus Alvarez Ruiz <jalvarezruiz@gmail.com>
2021-04-02 08:44:44 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showArrow(ArrowNames.South)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showArrow(ArrowNames.North)
+})
